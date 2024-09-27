74f2504bc4c865843527946afb78385afa6357af
sahil030804 <sahilranpariya60@gmail.com>
2024-09-27 16:09:13 +0530

    feat: logger middleware

--- server2.js
+++ server2.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import persons from "./routes/persons.js";
+import logger from "./Middleware/logger.js";
 import path from "path";
 import url from "url";
 let PORT = process.env.PORT;
@@ -13,6 +14,8 @@ app.use(express.json()); // raw body parser
 app.use(express.urlencoded({ extended: false })); // urlencoded turn to false
 
 // app.use(express.static(path.join(__dirname, "public")));
+
+app.use(logger);
 
 app.use("/api/persons", persons); // use /api/persons in url staticly for using all methods by only writing params & query into it at last
 
